20ed9b32a30ab44e5a83d4db74c7e24763ee4831
FrancisHutchings <Microsoft@FrancisHutchings.co.uk>
2016-03-21 19:38:49 +0000

    Fixing live

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ if ($_SERVER['APPLICATION_ENV'] == 'development') {
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 } else {
-    #error_reporting(0);
+    error_reporting(0);
     #ini_set("display_errors", 0);
     error_reporting(E_ALL ^ E_WARNING);
     ini_set('max_execution_time', 300);
